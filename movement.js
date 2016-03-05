a500b2c4e7277717a23e3248f4daf8b6d6e1f559
dscdac <davidsantiagocarrion@gmail.com>
2016-03-05 17:49:23 +0100

    Added camera.js

--- movement.js
+++ movement.js
@@ -8,53 +8,53 @@ pc.script.attribute('speed', 'number', 0.1, {
 
 // Script Definition
 pc.script.create('movement', function (app) {
-    
+
     // Creates a new Movement instance
     var Movement = function (entity) {
         this.entity = entity;
         this.force = new pc.Vec3();
     };
 
-    Movement.prototype = {        
+    Movement.prototype = {
         // Called every frame, dt is time in seconds since last update
         update: function (dt) {
             var forceX = 0;
             var forceZ = 0;
-            
+
             // calculate force based on pressed keys
             if (app.keyboard.isPressed(pc.KEY_LEFT)) {
                 forceX = -this.speed;
-            } 
-            
+            }
+
             if (app.keyboard.isPressed(pc.KEY_RIGHT)) {
                 forceX += this.speed;
             }
-            
+
             if (app.keyboard.isPressed(pc.KEY_UP)) {
                 forceZ = -this.speed;
-            } 
-            
+            }
+
             if (app.keyboard.isPressed(pc.KEY_DOWN)) {
                 forceZ += this.speed;
             }
-                        
+
             this.force.x = forceX;
             this.force.z = forceZ;
-            
+
             // if we have some non-zero force
             if (this.force.length()) {
-                
+
                 // calculate force vector
                 var rX = Math.cos(-Math.PI * 0.25);
                 var rY = Math.sin(-Math.PI * 0.25);
                 this.force.set(this.force.x * rX - this.force.z * rY, 0, this.force.z * rX + this.force.x * rY);
-                
+
                 // clamp force to the speed
                 if (this.force.length() > this.speed) {
                     this.force.normalize().scale(this.speed);
                 }
             }
-            
+
             // apply impulse to move the entity
             this.entity.rigidbody.applyImpulse(this.force);
         }
@@ -62,3 +62,9 @@ pc.script.create('movement', function (app) {
 
     return Movement;
 });
+
+
+/*
+var pos = this.entity.getPosition();
+console.log("X: " + pos.x + " Y: " + pos.y + " Z: " + pos.z);
+*/
